81b687b2b2189c4afb5fe0a676011d110f508280
Sophie <sophieolushola02@gmail.com>
2025-06-23 05:51:38 +0100

    updated

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-def island_perimeter(grid):
-    """
-    Returns the perimeter of the island in the grid.
 
-    Args:
-        grid (List[List[int]]): A list representing the grid, where
-                                0 represents water and 1 represents land.
+"""
+Defines the function island_perimeter
+"""
 
-    Returns:
-        int: The perimeter of the island.
-    """
-    perimeter = 0
+
+def boundary_squares(grid, i, j):
+    """this function calculates the number of boundary
+    squares"""
+    count = 0
     rows = len(grid)
-    colmn = len(grid[0]) if rows else 0
+    colmn = len(grid[0])
+    if (i > 0 and grid[i-1][j] == 1):
+        count += 1
+    if (j > 0 and grid[i][j-1] == 1):
+        count += 1
+    if (i < rows-1 and grid[i+1][j] == 1):
+        count += 1
+    if (j < colmn-1 and grid[i][j+1] == 1):
+        count += 1
+    return count
 
-    for i in range(rows):
-        for j in range(colmn):
+
+def island_perimeter(grid):
+    """this function returns the perimeter of an island
+    described with grid"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
+                perimeter += 4 - boundary_squares(grid, i, j)
     return perimeter
